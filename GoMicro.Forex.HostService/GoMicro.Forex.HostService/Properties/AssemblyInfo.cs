c3a8dfce12fc0822bb75e02349ca67b672c49b38
George Albertyn <george.albertyn@gmail.com>
2017-03-11 13:56:00 +0200

    Assebly Info

--- GoMicro.Forex.HostService/GoMicro.Forex.HostService/Properties/AssemblyInfo.cs
+++ GoMicro.Forex.HostService/GoMicro.Forex.HostService/Properties/AssemblyInfo.cs
@@ -2,21 +2,15 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("GoMicro.Forex.HostService")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Forex Micro Service")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("GoMicro")]
 [assembly: AssemblyProduct("GoMicro.Forex.HostService")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
