19c1ff4c1914438e600773b8e7e61ceee538bcd9
namer315 <namer315@yahoo.com>
2017-04-02 11:44:48 +0200

    Update PingController.cs

--- GoMicro.Owin/GoMicro.Forex.HostService/PingController.cs
+++ GoMicro.Owin/GoMicro.Forex.HostService/PingController.cs
@@ -2,9 +2,13 @@
 
 namespace GoMicro.Forex.HostService
 {
+    [RoutePrefix("Ping/")]
     public class PingController : ApiController
     {
         [HttpGet]
+        [Route("index")]
         public string Get() { return "Pong"; }
+        // url testing for postman
+        //http://localhost:9000/api/ping/index
     }
 }
